e3be1f5b15bf033de5e77ed4002f4aa9cd85a0ac
DELL <DELL@DESKTOP-KFABMG6>
2025-08-10 12:36:57 +0530

    firstcommit

---
+++ src/typecasting/typecasting_ex.java
@@ -0,0 +1,24 @@
+package typecasting;
+
+public class typecasting_ex {
+	public static void main(String[] args) {
+		
+		//widening 
+		int quatity=3;
+		double priceperitem=9.5;
+		double totalprice= quatity*priceperitem;
+		
+		//narrowing
+		
+		double discount=1.5;
+		int roundedDiscount =  (int)discount;
+		
+		double finalAmount =totalprice-roundedDiscount;
+		
+		System.out.println("Online Shopping summary");
+		System.out.println("final amount "+finalAmount);
+	}
+
+}
+
+
