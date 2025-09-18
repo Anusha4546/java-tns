4a3d922cc246d973bb2d610955492f7cf13329e5
DELL <DELL@DESKTOP-KFABMG6>
2025-09-18 21:02:15 +0530

    first commit

---
+++ src/corejava_tns/Method_Overloading.java
@@ -0,0 +1,29 @@
+package corejava_tns;
+
+
+
+class Calculator{
+	int add(int a,int b) {
+		return a+b;
+	}
+	double add(double a,double b) {
+		return a+b;
+	}
+	int add(int a,int b,int c) {
+		return a+b+c;
+	}
+	
+	
+}
+
+public class Method_Overloading {
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+		System.out.println("add(double,double)->"+cal.add(3,4));
+		System.out.println("add(int,int,int)->"+cal.add(1,4,9));
+		System.out.println("add(int,int)->"+cal.add(1,9));
+
+	}
+
+}
