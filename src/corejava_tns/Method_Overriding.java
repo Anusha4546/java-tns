f611e26466c4459c994d644c944c750d08817817
DELL <DELL@DESKTOP-KFABMG6>
2025-09-18 21:06:26 +0530

    first commit

---
+++ src/corejava_tns/Method_Overriding.java
@@ -0,0 +1,58 @@
+package corejava_tns;
+
+
+
+
+class Payment
+{
+	void makePayment()
+	{
+		System.out.println("Processing payment using generic method");
+	}
+}
+//subclass 
+
+class UpiPayment extends  Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using UPI");
+	}
+}
+
+class CardPayment extends Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using Credit/debit card");
+	}
+}
+
+
+class Walletpayment extends Payment
+{
+	void makePayment()
+	{
+		System.out.println("Payment made using Digital Wallet");
+	}
+}
+
+
+
+
+public class Method_Overriding {
+
+	public static void main(String[] args) {
+Payment payment;//reference of parent class
+		
+		payment=new UpiPayment();
+		payment.makePayment();
+		
+		payment =new CardPayment();
+		payment.makePayment();
+		
+		payment=new Walletpayment();
+		payment.makePayment();
+	}
+
+}
